dd1d57bc45697d0f0889d237fc329f55cb545f4a
clever-junior <tk_clever.junior@outlook.com>
2023-03-17 16:52:02 -0400

    feat: add requisite 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
-def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+def txt_importer(path_file: str):
+    if not path_file.endswith(".txt"):
+        raise FileNotFoundError("Formato inválido")
+
+    try:
+        with open(path_file) as txt_file:
+            lines = txt_file.readlines()
+
+        for line in lines:
+            line.replace("\\n", "\n")
+
+        return lines
+
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Arquivo {path_file} não encontrado")
--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -9,11 +9,11 @@ class Queue(AbstractQueue):
         return len(self._data)
 
     def enqueue(self, value):
-        self._data.append(value) 
-    
+        self._data.append(value)
+
     def dequeue(self):
         return self._data.pop(0)
-    
+
     def search(self, index):
         if index < 0:
             raise IndexError
